4758a4643262c2eeef690ca848621612e08c6456
Elias Symons <eisymonsmusic@gmail.com>
2022-12-13 13:21:02 -0800

    updated contact info

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,10 +4,10 @@ import email from "../images/Subtract.png";
 function Socials() {
   return (
     <div className="socials">
-      <a href="tel:123-456-7890">
+      <a href="tel:828-850-2852">
         <img alt="phone" className="phone-icon" src={phone} />
       </a>
-      <a href="mailto: abc@example.com">
+      <a href="mailto: classiguitarist@gmail.com">
         <img alt="email" className="email-icon" src={email} />
       </a>
     </div>
